a6d079ed722b1979f220fbe2e72cb44412b9b6e1
drlc <daraloca@live.it>
2023-12-12 23:41:30 +0100

    fix: cors

--- be/src/main/java/it/daraloca/ice/takehometask/configuration/ApiCnfg.java
+++ be/src/main/java/it/daraloca/ice/takehometask/configuration/ApiCnfg.java
@@ -1,12 +1,29 @@
 package it.daraloca.ice.takehometask.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "it.daraloca.ice.takehometask.endpoint" })
-public class ApiCnfg {
+public class ApiCnfg{
+
+    @Value("${api.cors.alloworigin:*}")
+    private String allowOrigin;
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*").allowedOrigins(allowOrigin);
+			}
+		};
+	}
 
 }
